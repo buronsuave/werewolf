0e25044de5a0dc309246fcd84cf3e8d662068545
AlexaKarime <a23310188@ceti.mx>
2024-05-26 03:18:32 -0600

    second commit and commit with daniel´s changes

--- game/GameServer.h
+++ game/GameServer.h
@@ -1,28 +1,163 @@
-#ifndef GAME_SERVER_H
-#define GAME_SERVER_H
+#include "GameClient.h"
 
-#include "common.h"
-#include "Player.h"
-
-class GameServer : public Server
+void GameClient::handle_recv(char buf[], SOCKET s)
 {
-    private:
-    STAGE stage = STAGE_NEW;
+    switch (stage)
+    {
+        case STAGE_LOBBY:
+        {
+            if (strstr(buf, GAME_EVENT_MAINHOST))
+            {
+                printf("You're the main host\n");
+                printf("You have the power to start the play!\n");
+                printf("Type your name, and when you get ready press enter to start the play.\n");
+                char response[DEFAULT_BUFLEN];
+                char name[DEFAULT_BUFLEN];
+                strcpy(response, GAME_EVENT_INIT);
+                std::cin.getline(name, DEFAULT_BUFLEN);
+                strcat(response, name);
 
-    public:
-    std::vector<Player> players;
-    
-    void handle_recv(fd_set master, int fdmax, int listener, int i, char buf[], int nbytes);
+                send_message(s, response, DEFAULT_BUFLEN);
+                return;
+            }
+            // TODO: lobby,jugador1,jugador2,jugador3
+            else if (strstr(buf, GAME_EVENT_LOBBY)) // TODO: Checar si GAME_EVENT_LOBBY esta contenida en el buf
+            {
+                // TODO: MOSTRAR LOS JUGADORES QUE SE HAN UNIDO
+                printf("You've just jumped to the lobby.\nWaiting for main host to start.\n");
+                printf("Please enter your name: ");
+                char response[DEFAULT_BUFLEN];
+                char name[DEFAULT_BUFLEN];
+                strcpy(response, GAME_EVENT_NAME);
+                std::cin.getline(name, DEFAULT_BUFLEN);
+
+                strcat(response, name);
+                send_message(s, response, DEFAULT_BUFLEN);
+                return;
+            }
+            else if (strstr(buf, GAME_EVENT_START))
+            {
+                printf("Main host started the play!\n");
+                write_formatted_log(GRAY "[CLIENT LOG] Changing state to NIGHT\n" RESET);
+                stage = STAGE_ROLE;
+                return;
+            }
+            // Add case for loading card information
+            else
+            {
+                write_formatted_log(GRAY "[CLIENT LOG] Unable to handle message\n" RESET);
+                return;
+            }
+
+            break;
+        }
+
+        case STAGE_ROLE:
+        {
+             if (strstr(buf,GAME_EVENT_ROLE ))
+             {
+               //LOGICA PARA EXTRAER ROLE 
+             }
+             else {
+                printf("Error fatal");
+             }
 
 
-   void AssignROLE();
+        }
+        case STAGE_NIGHT:
+        {
+            if(strstr(buf, GAME_EVENT_ACTION_WORELOF))
+          {
+            //ESPERAR ENTRADA DE USUARIO (VOTO)
+           // ENVIAR A SERVIDOR "VOTO LOBO" + VOTO
+           return;
+          }
+          if (strstr(buf, GAME_EVENT_ACTION_SEER))
+          {
+            //EXTRAER RESPUESTA DESDE EL MENSAJE
+            //MOSTRAR RESPUESTA
+            return;
+          }
+          if (strstr(buf, GAME_EVENT_ACTION_WITCH))
+          {
+            // ESPERAR ENTRADA DE USUARIO (JUGADOR, ACCION)
+            // ENVIAR A SERVIDOR "POCION BRUJA" + (JUGADOR, ACCION)
+            return;
+          }
+         
+          if(strstr(buf,GAME_EVENT_DAY))
+          {
+            stage = STAGE_ROLE;
+            return;
+          }
+          // si no error 
+          else {
+            printf("Fatal error");
+          }
 
-    
-
-   
-
-
-};
-
-
-#endif
+    }
+        case STAGE_DAY:
+        {
+         if(strstr(buf,GAME_EVENT_DAY))
+         {
+                //EXTRAER MUERTOS DESDE EL MENSAJE
+                //MOSTRAR MUERTOS
+                //SI ESTOY EN MUERTOS Y NO SOY CAZADOR
+                stage = STAGE_VIEWER;
+                return;
+         }
+         if(strstr(buf,GAME_EVENT_NEW))
+         {
+           // EXTRAER MUERTOS DESDE EL MENSAJE
+           // MOSTRAR MUERTOS
+           // SI ESTOY EN MUERTOS Y NO SOY CAZADOR
+         //{
+             stage = STAGE_VIEWER;
+         //}
+         }
+         if(strstr(buf,GAME_EVENT_ACTION_HUNTER1))
+         {
+            //ESPERAR ENTRADA DE USUARIO (JUGADOR)
+            //ENVIAR A SERVIDOR "VICTIMA CAZADOR 1" + JUGADOR 
+            stage=STAGE_VIEWER;
+            return;
+         }
+         if(strstr(buf,GAME_EVENT_ACTION_HUNTER2))
+         {
+           //ESPERAR ENTRADA DE USUARIO (JUGADOR)
+           //ENVIAR A SERVIDOR "VICTIMA CAZADOR 2" + JUGADOR
+           stage = STAGE_VIEWER;
+            return;
+         }
+         if(strstr(buf,GAME_EVENT_VICTIM_HUNTER))
+         {
+              //EXTRAER VICTIMA DESDE MENSAJE
+              //MOSTRAR VICTIMA
+              //SI SOY VICTIMA
+               stage = STAGE_VIEWER;
+            return;
+         }
+         if(strstr(buf,GAME_EVENT_ACTION_EVERYONE))
+         {
+            //ESPERAR ENTRADA DE USUARIO (VOTO)
+            //ENVIAR A SERVIDOR "VOTO JUGADOR" + VOTO
+            return;
+         }
+         if(strstr(buf,GAME_EVENT_NIGHT))
+         {
+            stage = STAGE_NIGHT;
+            return;
+         }
+         if(strstr(buf,GAME_EVENT_GAMEOVER))
+         {
+            //EXTRAER GANADOR DESDE EL MENSAJE
+            //MOSTRAR GANADOR
+            stage = STAGE_GAME_OVER;
+            return; //fin del juego
+         }
+         else
+         {
+            printf("Fatal error");
+        } 
+    }
+}
--- game/common.h
+++ game/common.h
@@ -12,7 +12,8 @@ enum STAGE
     STAGE_OVER = 3,
     STAGE_NEW = 4,
     STAGE_VIEWER= 5,
-    STAGE_ROLE = 6    
+    STAGE_ROLE = 6,  
+    STAGE_GAME_OVER = 7  
 };
 
 enum ROLE
@@ -33,6 +34,17 @@ enum ROLE
 #define GAME_EVENT_BADREQUEST "badrequest"
 #define GAME_EVENT_NAME "name"
 #define GAME_EVENT_ROLE "role"
+#define GAME_EVENT_ACTION_WORELOF "worelof"
+#define GAME_EVENT_ACTION_WITCH "witch"
+#define GAME_EVENT_ACTION_SEER "seer"
+#define GAME_EVENT_VICTIM_HUNTER "cazador"
+#define GAME_EVENT_ACTION_HUNTER2 "hunter2"
+#define GAME_EVENT_ACTION_HUNTER1 "hunter1"
+#define GAME_EVENT_ACTION_EVERYONE "hunter1"
+#define GAME_EVENT_NIGHT "nigth"
+#define GAME_EVENT_DAY "day"
+#define GAME_EVENT_NEW "new"
+#define GAME_EVENT_GAMEOVER "gameover"
 
 #define GAME_MAX_PLAYERS 20
 #define GAME_MIN_PLAYERS 8
